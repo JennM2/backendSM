b1614af95c53fba29e6ae194bc3cbec9777b45b4
jesusm01 <3mj.m20@gmail.com>
2024-06-20 00:46:16 -0400

    1.06

--- src/server.js
+++ src/server.js
@@ -40,11 +40,11 @@ async function startServer(){
 
             pool.getConnection((err, connection) => {
               console.log({
-                port : process.env.DB_PORT,
-                host: process.env.DB_HOST,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_NAME,
+                port : process.env.MYSQLPORT,
+                host: process.env.MYSQLHOST,
+                user: process.env.MYSQLUSER,
+                password: process.env.MYSQLPASSWORD,
+                database: process.env.MYSQLDATABASE,
                 connectionLimit: 10, // Establecer el límite máximo de conexiones
                 queueLimit: 0,
               })
