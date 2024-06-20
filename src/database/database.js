257c822608ef1cdc754376ef3c2a2e2b4defc325
jesusm01 <3mj.m20@gmail.com>
2024-06-20 01:48:52 -0400

    1.2

--- src/database/database.js
+++ src/database/database.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 
 const pool = mysql.createPool({
-    port : process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    port : process.env.MYSQLPORT,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     connectionLimit: 10, // Establecer el límite máximo de conexiones
     queueLimit: 0,
 });
